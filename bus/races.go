7d0014be6f25313be289c384a926f3768dbc1b39
vo13crabs <vladimir.fodorow@gmail.com>
2022-03-19 17:01:07 +0700

    add all orders methods

--- bus/races.go
+++ bus/races.go
@@ -125,6 +125,122 @@ type RaceSummaryDTO struct {
 	} `json:"ticketTypes"`
 }
 
+type SaleDTO struct {
+	Birthday       time.Time `json:"birthday"`
+	Citizenship    string    `json:"citizenship"`
+	DocNum         string    `json:"docNum"`
+	DocSeries      string    `json:"docSeries"`
+	DocTypeCode    string    `json:"docTypeCode"`
+	FirstName      string    `json:"firstName"`
+	Gender         string    `json:"gender"`
+	LastName       string    `json:"lastName"`
+	MiddleName     string    `json:"middleName"`
+	Phone          string    `json:"phone"`
+	SeatCode       string    `json:"seatCode"`
+	TicketTypeCode string    `json:"ticketTypeCode"`
+}
+
+type BusOrderDTO struct {
+	Agent struct {
+		Extra    int    `json:"extra"`
+		FullName string `json:"fullName"`
+		Id       int    `json:"id"`
+		Markup   int    `json:"markup"`
+	} `json:"agent"`
+	Created       time.Time `json:"created"`
+	Expired       time.Time `json:"expired"`
+	Finished      time.Time `json:"finished"`
+	Id            int       `json:"id"`
+	PaymentMethod string    `json:"paymentMethod"`
+	Repayment     int       `json:"repayment"`
+	ReserveCode   string    `json:"reserveCode"`
+	Status        string    `json:"status"`
+	Tickets       []struct {
+		ArrivalAddress    string    `json:"arrivalAddress"`
+		ArrivalDate       time.Time `json:"arrivalDate"`
+		ArrivalStation    string    `json:"arrivalStation"`
+		Barcode           string    `json:"barcode"`
+		Birthday          time.Time `json:"birthday"`
+		BusInfo           string    `json:"busInfo"`
+		Carrier           string    `json:"carrier"`
+		CarrierInn        string    `json:"carrierInn"`
+		Citizenship       string    `json:"citizenship"`
+		DispatchAddress   string    `json:"dispatchAddress"`
+		DispatchDate      time.Time `json:"dispatchDate"`
+		DispatchStation   string    `json:"dispatchStation"`
+		DocNum            string    `json:"docNum"`
+		DocSeries         string    `json:"docSeries"`
+		DocType           string    `json:"docType"`
+		DownloadUrl       string    `json:"downloadUrl"`
+		Dues              int       `json:"dues"`
+		FirstName         string    `json:"firstName"`
+		Gender            string    `json:"gender"`
+		Id                int       `json:"id"`
+		LastName          string    `json:"lastName"`
+		MiddleName        string    `json:"middleName"`
+		Phone             string    `json:"phone"`
+		Platform          string    `json:"platform"`
+		Price             int       `json:"price"`
+		RaceClassId       int       `json:"raceClassId"`
+		RaceName          string    `json:"raceName"`
+		RaceNum           string    `json:"raceNum"`
+		RaceUid           string    `json:"raceUid"`
+		Repayment         int       `json:"repayment"`
+		Returned          time.Time `json:"returned"`
+		Seat              string    `json:"seat"`
+		Status            string    `json:"status"`
+		SupplierDues      int       `json:"supplierDues"`
+		SupplierFare      int       `json:"supplierFare"`
+		SupplierPrice     int       `json:"supplierPrice"`
+		SupplierRepayment int       `json:"supplierRepayment"`
+		TicketClass       string    `json:"ticketClass"`
+		TicketCode        string    `json:"ticketCode"`
+		TicketNum         string    `json:"ticketNum"`
+		TicketSeries      string    `json:"ticketSeries"`
+		TicketType        string    `json:"ticketType"`
+		Updatable         bool      `json:"updatable"`
+		Vat               int       `json:"vat"`
+	} `json:"tickets"`
+	Total int `json:"total"`
+}
+
+type TourDTO struct {
+	First struct {
+		RaceUid string `json:"raceUid"`
+		Sales   []struct {
+			Birthday       time.Time `json:"birthday"`
+			Citizenship    string    `json:"citizenship"`
+			DocNum         string    `json:"docNum"`
+			DocSeries      string    `json:"docSeries"`
+			DocTypeCode    string    `json:"docTypeCode"`
+			FirstName      string    `json:"firstName"`
+			Gender         string    `json:"gender"`
+			LastName       string    `json:"lastName"`
+			MiddleName     string    `json:"middleName"`
+			Phone          string    `json:"phone"`
+			SeatCode       string    `json:"seatCode"`
+			TicketTypeCode string    `json:"ticketTypeCode"`
+		} `json:"sales"`
+	} `json:"first"`
+	Second struct {
+		RaceUid string `json:"raceUid"`
+		Sales   []struct {
+			Birthday       time.Time `json:"birthday"`
+			Citizenship    string    `json:"citizenship"`
+			DocNum         string    `json:"docNum"`
+			DocSeries      string    `json:"docSeries"`
+			DocTypeCode    string    `json:"docTypeCode"`
+			FirstName      string    `json:"firstName"`
+			Gender         string    `json:"gender"`
+			LastName       string    `json:"lastName"`
+			MiddleName     string    `json:"middleName"`
+			Phone          string    `json:"phone"`
+			SeatCode       string    `json:"seatCode"`
+			TicketTypeCode string    `json:"ticketTypeCode"`
+		} `json:"sales"`
+	} `json:"second"`
+}
+
 func (b *bus) GetRaces(ctx context.Context, fromID, toID int, date string, count int) (*[]RaceDTO, error) {
 	v := url.Values{
 		"from":  []string{strconv.Itoa(fromID)},
@@ -149,6 +265,20 @@ func (b *bus) GetRaceSummary(ctx context.Context, raceUID string) (*RaceSummaryD
 	return raceSummary, nil
 }
 
-// TODO: бронирование билетов на рейс
+func (b *bus) OrderOnOneRace(ctx context.Context, raceUID string, sales []SaleDTO, accessToken string) (*BusOrderDTO, error) {
+	u := b.createUrl(fmt.Sprintf("/v1/races/%s/orders", raceUID), nil)
+	busOrder := &BusOrderDTO{}
+	if err := requests.PostRequest(ctx, u, sales, busOrder, auth(accessToken)); err != nil {
+		return nil, err
+	}
+	return busOrder, nil
+}
 
-// TODO: бронирование билетов на 2 рейса
+func (b *bus) OrderOnTwoRace(ctx context.Context, tourDTO TourDTO, accessToken string) (*BusOrderDTO, error) {
+	u := b.createUrl("/v1/races/orders", nil)
+	busOrder := &BusOrderDTO{}
+	if err := requests.PostRequest(ctx, u, tourDTO, busOrder, auth(accessToken)); err != nil {
+		return nil, err
+	}
+	return busOrder, nil
+}
